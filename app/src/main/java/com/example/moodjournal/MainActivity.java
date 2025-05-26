5eb63817e8ca311885b7d8fc1b67f2307b78c15b
scjm1014 <charizzjirehseno@gmail.com>
2025-05-26 18:55:08 +0800

    added to main activity

---
+++ app/src/main/java/com/example/moodjournal/MainActivity.java
@@ -0,0 +1,9 @@
+private void openHistoryActivity() {
+    Intent intent = new Intent(this, HistoryActivity.class);
+    startActivity(intent);
+}
+
+private void openSettingsActivity() {
+    Intent intent = new Intent(this, SettingsActivity.class);
+    startActivity(intent);
+}
